b8c33e89099d8e698a518685c16fa5bdd7907891
Stephane Eybert <mittiprovence@yahoo.se>
2014-11-11 16:42:28 +0100

    Added missing socket source and help files.

---
+++ api/js/socket/server.js
@@ -0,0 +1,72 @@
+var http = require('http');
+var connect = require('connect');
+var cookie = require('cookie');
+var socketio = require('socket.io');
+//var sessionSocketio = require('session.socket.io');
+var redis = require('redis')
+
+var utils = require('./utils.js');
+
+var PORT = 9001;
+var portNumber = process.argv[2] || PORT;
+
+var httpServer = http.createServer(utils.httpHandler);
+
+module.exports.io = socketio.listen(httpServer).configure(function () {
+  this.enable('browser client minification');  // send minified client
+  this.enable('browser client etag');          // apply etag caching logic based on version number
+  this.enable('browser client gzip');          // gzip the file
+  this.set('log level', 1);                    // reduce logging
+  this.set('transports', [                     // enable all transports (optional if you want flashsocket)
+    'websocket',
+    'flashsocket',
+    'htmlfile',
+    'xhr-polling',
+    'jsonp-polling'
+    ]);
+});
+
+// By default configuration, socket.io uses the memory (MemoryStore) to hold open connections. Hence it is not possible to run several socket.io processes, because the processes won't know nothing about the open connections of the other processes. But it's quite easy to use redis as store, which allows to share the open connections between several socket.io processes, which run on different ports.
+var redisClient = redis.createClient();
+var pub = redis.createClient();
+var sub = redis.createClient();
+var RedisStore = require('socket.io/lib/stores/redis');
+var redisStore = new RedisStore({
+  ttl: 1800,
+  redisPub: pub,
+  redisSub: sub,
+  client:redisClient
+});
+module.exports.io.set('store', redisStore);
+
+httpServer.listen(portNumber, function() {
+  console.log('The NodeJS http server is listening...');
+});
+
+module.exports.io.set('authorization', function (handshakeData, handler) {
+  if (handshakeData.headers.cookie) {
+    handshakeData.cookies = cookie.parse(decodeURIComponent(handshakeData.headers.cookie));
+    handshakeData.sessionID = handshakeData.cookies['PHPSESSID'];
+    handshakeData.socketSessionId = handshakeData.cookies['socketSessionId'];
+    console.log("Authorization with sessionID: " + handshakeData.sessionID);
+    console.log("Authorization with socketSessionId: " + handshakeData.socketSessionId);
+    redisClient.get("PHPREDIS_SESSION:" + handshakeData.sessionID, function (error, reply) {
+      if (error) {
+        console.log("The redis client had an error: " + error);
+        return handler('The connection was refused because the redis client had an error.', false);
+      } else if (!reply) {
+        return handler('The connection was refused because the redis client did not find the session id.', false);
+      } else {
+        var redisSocketSessionId = utils.getRedisValue(reply, "socketSessionId");
+        if ('undefined' == typeof handshakeData.socketSessionId || redisSocketSessionId != handshakeData.socketSessionId) {
+          return handler('The connection was refused because the session id was invalid.', false);
+        } else {
+          handler(null, true);
+        }
+      }
+    });
+  } else {
+    return handler('The connection was refused because no cookie was transmitted.', false);
+  }
+});
+
---
+++ api/js/socket/utils.js
@@ -0,0 +1,66 @@
+var formidable = require('formidable');
+
+Array.prototype.contains = function(k, callback) {
+  var self = this;
+  return (function check(i) {
+    if (i >= self.length) {
+      return callback(false);
+    }
+    if (self[i] === k) {
+      return callback(true);
+    }
+    return process.nextTick(check.bind(null, i+1));
+  }(0));
+};
+
+module.exports.isEmpty = function(obj) {
+  for(var prop in obj) {
+    if(obj.hasOwnProperty(prop))
+      return false;
+  }
+  return true;
+}
+
+module.exports.getRedisValue = function(data, name) {
+  var redisBits = data.split(";");
+  for (var i in redisBits) {
+    if (redisBits.hasOwnProperty(i)) {
+      if (redisBits[i].substring(0, name.length) == name) {
+        var value = redisBits[i].split("|")[1].split(":")[2].replace("\"", "").replace("\"", "");
+        return(value);
+      }
+    }
+  }
+};
+
+// Handle http requests sent to the Node.js server
+module.exports.httpHandler = function(req, res) {
+  switch(req.url) {
+    case '/ping':
+      if (req.method == 'GET') {
+        console.log("[200] " + req.method + " to " + req.url);
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('');
+      }
+      break;
+    case '/push':
+      if (req.method == 'POST') {
+        console.log("[200] " + req.method + " to " + req.url);
+        form = new formidable.IncomingForm();
+        form.parse(req, function(e, fields, files) {
+          res.writeHead(200, {'Content-Type': 'text/plain'});
+          res.end('');
+          httpHandleServerPostRequest(fields);
+        });
+      }
+      break;
+    default:
+      send404(res);
+  };
+};
+
+send404 = function(res) {
+  res.writeHead(404);
+  res.write('404');
+  res.end();
+};
