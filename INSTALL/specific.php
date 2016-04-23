5d695fb08c20dea273257861a904274f033b6e3f
Stephane Eybert <mittiprovence@yahoo.se>
2016-04-23 22:42:22 +0300

    The website details file.

---
+++ INSTALL/specific.php
@@ -0,0 +1,20 @@
+<?PHP
+
+// The website specific data
+
+// The name of the web site
+$gSetupWebsiteName = 'learnintouch';
+
+// The name of the web site url
+$gSetupWebsiteUrl = 'http://dev.learnintouch.com';
+
+// The root path
+$gRootPath = '/home/stephane/dev/php/learnintouch/www/learnintouch.com/';
+
+// The database for one website
+define('DB_HOST', "localhost");
+define('DB_NAME', "learnintouch");
+define('DB_USER', "learnintouch");
+define('DB_PASS', "mignet");
+
+?>
