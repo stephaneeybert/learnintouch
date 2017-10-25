19f1ab7c1830665c6d31a48a5704665228309d6c
Stephane Eybert <mittiprovence@yahoo.se>
2017-10-25 14:50:32 +0200

    Make the label more explicit.

--- modules/elearning/exercise/.audio.en.php
+++ modules/elearning/exercise/.audio.en.php
@@ -1,8 +1,8 @@
 [   0]Upload an audio file
-[   1]An exercise can have an audio file.\n\nThe audio file is played before the user answers the exercise questions.
+[   1]An exercise can have an audio file.\n\nThe audio file is played before the user answers the exercise questions.\n\nIt is possible to start playing the audio file automatically.
 [   2]Kb.
 [   3]File name:
-[   4]Autostart:
+[   4]Start playing automatically:
 [   5]Select a file:
 [   6]Audio file:
 [   7]Delete the audio file?
--- modules/elearning/exercise/.audio.fr.php
+++ modules/elearning/exercise/.audio.fr.php
@@ -1,8 +1,8 @@
 [   0]Télécharger un fichier audio
-[   1]Un exercice peut avoir un fichier audio.\n\nLe fichier audio est joué avant que l'utilisateur ne réponde aux questions de l'exercice.
+[   1]Un exercice peut avoir un fichier audio.\n\nLe fichier audio est joué avant que l'utilisateur ne réponde aux questions de l'exercice.\n\nIl est possible de démarrer automatiquement la lecture du fichier audio.
 [   2]Ko.
 [   3]Nom du fichier:
-[   4]Autostart:
+[   4]Démarrer la lecture automatiquement:
 [   5]Sélectionner un fichier:
 [   6]Fichier audio:
 [   7]Effacer le fichier audio?
