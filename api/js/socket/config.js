3962eb22d1c18c49ab4e68cf344a27f65dc996c5
Stephane Eybert <mittiprovence@yahoo.se>
2017-10-25 14:01:20 +0200

    Corrected a wrong name of a property.

--- api/js/socket/config.js
+++ api/js/socket/config.js
@@ -1,11 +1,10 @@
 var config = {};
 
 config.redis = {};
-config.redis.host = 'localhost';
+config.redis.hostname = 'redis';
 config.redis.port = 6379;
 
 config.socketio = {};
-config.socketio.host = 'localhost';
 config.socketio.port = 9001;
 
 module.exports = config;
