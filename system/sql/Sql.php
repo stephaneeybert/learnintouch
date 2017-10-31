cf13eccc4a8cd415f425ae07a1da304067afe481
Stephane Eybert <mittiprovence@yahoo.se>
2017-10-31 09:39:13 +0100

    Have more details in the error messages.

--- system/sql/Sql.php
+++ system/sql/Sql.php
@@ -12,7 +12,7 @@ class Sql {
     $dataSource = new DataSourceMySQL(DB_HOST, DB_PORT, $dbName);
 
     if (!$dataSource->connect(DB_USER, DB_PASS)) {
-      die("The data source for the database $dbName could not be initialized for the user " . DB_USER . ".");
+      die("The data source for the database $dbName could not be initialized for the user " . DB_USER . " on the host " . DB_HOST . ":" . DB_PORT);
     }
 
     return($dataSource);
@@ -28,7 +28,7 @@ class Sql {
     $dataSource = new DataSourceMySQL(DB_COMMON_HOST, DB_COMMON_PORT, $dbName);
 
     if (!$dataSource->connect(DB_COMMON_USER, DB_COMMON_PASS)) {
-      die("The data source for the database $dbName could not be initialized for the user " . DB_COMMON_USER . ".");
+      die("The data source for the database $dbName could not be initialized for the user " . DB_COMMON_USER . " on the host " . DB_COMMON_HOST . ":" . DB_COMMON_PORT);
     }
 
     return($dataSource);
