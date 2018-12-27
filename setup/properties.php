56e0bd012584278fbea3bd71cbe2df39113fcc5b
Stephane Eybert <mittiprovence@yahoo.se>
2018-12-27 13:16:24 +0200

    Added the file after having removed its git history.

---
+++ setup/properties.php
@@ -0,0 +1,15 @@
+<?PHP
+
+define('DB_COMMON_DB_NAME', getenv("LEARNINTOUCH_DB_NAME"));
+define('DB_COMMON_HOST', DB_HOST);
+define('DB_COMMON_PORT', DB_PORT);
+define('DB_COMMON_USER', getenv("LEARNINTOUCH_DB_USER"));
+define('DB_COMMON_PASS', getenv("LEARNINTOUCH_DB_PASSWORD"));
+
+define('STAFF_EMAIL', "mittiprovence@yahoo.se");
+
+define('LOCALHOST', 'http://localhost');
+
+define('NODEJS_SOCKET_PORT', 9001);
+
+?>
