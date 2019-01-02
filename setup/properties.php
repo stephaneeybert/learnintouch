24a535f1af617c0d69860997f381562b8bf923a3
Stephane Eybert <mittiprovence@yahoo.se>
2019-01-02 14:01:14 +0200

    Replaced some hard coded variables by environment variables.

--- setup/properties.php
+++ setup/properties.php
@@ -1,15 +1,15 @@
 <?PHP
 
 define('DB_COMMON_DB_NAME', getenv("LEARNINTOUCH_DB_NAME"));
-define('DB_COMMON_HOST', DB_HOST);
-define('DB_COMMON_PORT', DB_PORT);
+define('DB_COMMON_HOST', getenv("DB_HOST"));
+define('DB_COMMON_PORT', getenv("DB_PORT"));
 define('DB_COMMON_USER', getenv("LEARNINTOUCH_DB_USER"));
 define('DB_COMMON_PASS', getenv("LEARNINTOUCH_DB_PASSWORD"));
 
-define('STAFF_EMAIL', "mittiprovence@yahoo.se");
+define('STAFF_EMAIL', getenv("STAFF_EMAIL"));
 
-define('LOCALHOST', 'http://localhost');
+define('LOCALHOST', getenv("LOCAL_HOST"));
 
-define('NODEJS_SOCKET_PORT', 9001);
+define('NODEJS_SOCKET_PORT', getenv("NODEJS_SOCKET_PORT"));
 
 ?>
