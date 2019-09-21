4d4d77c81be85cc45bd31d45ba697924b67f1d74
Stephane Eybert <mittiprovence@yahoo.se>
2019-09-21 11:00:21 +0200

    Renamed the certificate files.

--- api/js/socket/config/development.js
+++ api/js/socket/config/development.js
@@ -7,6 +7,6 @@ config.ssl = {};
 config.ssl.path = '/usr/local/learnintouch/letsencrypt/';
 config.ssl.key = 'current-privkey.pem';
 config.ssl.certificate = 'current-cert.pem';
-config.ssl.chain = 'current-chain.pem';
+config.ssl.chain = 'current-fullchain.pem';
 
 module.exports = config;
