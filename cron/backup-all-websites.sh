5b349c52c66a453d8e90abfc84bf89a950c75025
Stephane Eybert <mittiprovence@yahoo.se>
2021-05-14 10:07:00 +0200

    Renamed the backup script file.

---
+++ cron/backup-all-websites.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+basedir=/usr/local/learnintouch/www
+
+cd $basedir/europasprak.com
+./backup.sh
+
+cd $basedir/fhs.europasprak.com
+./backup.sh
+
+#cd $basedir/learnintouch.com
+#./backup.sh
+
+#cd $basedir/thalasoft.com
+#./backup.sh
+
