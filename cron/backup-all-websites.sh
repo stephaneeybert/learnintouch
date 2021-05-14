2341b4d2306a991dd988df508f0760656272e6d8
Stephane Eybert <mittiprovence@yahoo.se>
2021-05-14 17:54:53 +0200

    Changed the cron backup to work in docker.

--- cron/backup-all-websites.sh
+++ cron/backup-all-websites.sh
@@ -3,14 +3,14 @@
 basedir=/usr/local/learnintouch/www
 
 cd $basedir/europasprak.com
-./backup.sh
+./backup.sh &
 
 cd $basedir/fhs.europasprak.com
-./backup.sh
+./backup.sh &
 
-#cd $basedir/learnintouch.com
-#./backup.sh
+cd $basedir/learnintouch.com
+./backup.sh &
 
 #cd $basedir/thalasoft.com
-#./backup.sh
+#./backup.sh &
 
--- cron/backup.sh
+++ cron/backup.sh
@@ -1,3 +1,16 @@
-#!/bin/bash
+#!/bin/bash -x
 
-/home/stephane/programs/install/bin/php -d include_path='/home/stephane/learnintouch/engine/setup/' -f /home/stephane/learnintouch/engine/system/cron/backup.php $PWD/account/setup/specific.php
+# Expand the secrets
+export DB_ROOT_PASSWORD={{DOCKER-SECRET:DB_ROOT_PASSWORD}}
+export LEARNINTOUCH_DB_NAME={{DOCKER-SECRET:LEARNINTOUCH_DB_NAME}}
+export LEARNINTOUCH_DB_USER={{DOCKER-SECRET:LEARNINTOUCH_DB_USER}}
+export LEARNINTOUCH_DB_PASSWORD={{DOCKER-SECRET:LEARNINTOUCH_DB_PASSWORD}}
+export WWW_LEARNINTOUCH_DB_PASSWORD={{DOCKER-SECRET:WWW_LEARNINTOUCH_DB_PASSWORD}}
+export WWW_EUROPASPRAK_DB_PASSWORD={{DOCKER-SECRET:WWW_EUROPASPRAK_DB_PASSWORD}}
+export WWW_FHS_DB_PASSWORD={{DOCKER-SECRET:WWW_FHS_DB_PASSWORD}}
+export WWW_FOLKUNIVERSITET_DB_PASSWORD={{DOCKER-SECRET:WWW_FOLKUNIVERSITET_DB_PASSWORD}}
+export WWW_THALASOFT_DB_PASSWORD={{DOCKER-SECRET:WWW_THALASOFT_DB_PASSWORD}}
+source /usr/local/learnintouch/expand-secrets.sh
+#/usr/local/learnintouch/expand-all-secrets.sh TODO DRY ~/dev/docker/projects/learnintouch/learnintouch/start.sh
+
+/usr/local/php/install/bin/php -d include_path='/usr/local/learnintouch/engine/setup/' -f /usr/local/learnintouch/engine/system/cron/backup.php $PWD/account/setup/specific.php
