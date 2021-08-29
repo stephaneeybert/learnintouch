6c13eb1d3a195bc8c9dc4facf91c15c4476a82e8
Stephane Eybert <mittiprovence@yahoo.se>
2021-08-29 13:32:39 +0200

    Updated the SSL certificate files locations.

--- api/js/socket/config/production.js
+++ api/js/socket/config/production.js
@@ -4,9 +4,9 @@ config.redis = {};
 config.redis.hostname = 'redis';
 
 config.ssl = {};
-config.ssl.path = '/usr/local/learnintouch/letsencrypt/';
-config.ssl.key = 'current-privkey.pem';
-config.ssl.certificate = 'current-cert.pem';
-config.ssl.chain = 'current-fullchain.pem';
+config.ssl.path = '/etc/letsencrypt/live/europasprak.com/';
+config.ssl.key = 'privkey.pem';
+config.ssl.certificate = 'cert.pem';
+config.ssl.chain = 'fullchain.pem';
 
 module.exports = config;
