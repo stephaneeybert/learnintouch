f429999950dbcc5720ad34008492381108e5286e
Stephane Eybert <mittiprovence@yahoo.se>
2021-10-03 15:37:34 +0200

    Added some missing env variables for the auto backup.

---
+++ INSTALL/setenv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+export WWW_EUROPASPRAK_NAME=europasprak
+export WWW_EUROPASPRAK_DOMAIN=www.europasprak.com
+export WWW_EUROPASPRAK_SCHEME=https
+
+export WWW_EUROPASPRAK_DB_NAME=db_europasprak
+export WWW_EUROPASPRAK_DB_USER=europasprak
+export WWW_EUROPASPRAK_DB_PASSWORD=TODO
+export DB_HOST=localhost
+export DB_PORT=3306
--- cron/backup-all-websites.sh
+++ cron/backup-all-websites.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-basedir=/usr/local/learnintouch/www
+basedir=/home/europasprak/dev/learnintouch
 
 # Entering each website home directory is required so as to point to its specific properties file
 
-cd $basedir/europasprak.com
-./backup.sh &
+cd $basedir/www.europasprak
+source ./setenv.sh
+./backup.sh
 
-cd $basedir/fhs.europasprak.com
-./backup.sh &
-
-cd $basedir/learnintouch.com
-./backup.sh &
-
-#cd $basedir/thalasoft.com
-#./backup.sh &
+cd $basedir/www.fhs
+source ./setenv.sh
+./backup.sh
 
